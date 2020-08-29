fe459941657487d83db06251456ed5393980915c
Rob Parker <jademonkey7@gmail.com>
2020-08-29 18:44:55 +0100

    read necessary info from files in data directory

--- data/README.md
+++ data/README.md
@@ -3,6 +3,8 @@
 This folder contains several files that are read which contains secrets and other information not safe for storing on GitHub
 
 * token - A file containing a single line consisting of the token the bot should use.
-* guildID - A file containing a single line consisting of the guildID of the server it is connected to.
+* guild - A file containing a single line consisting of the guildID of the server it is connected to.
 * admins - A list of user IDs that are users considered as Admins.
-* roleStrToRole - A list of string mappings that denote the text short role to the role ID for when users request to be added/removed from roles.
+* roles - A list of string mappings that denote the text short role to the role ID for when users request to be added/removed from roles.
+* bans - A list of banned users who cannot run commands anymore
+* channel - The file containing a single line consisting of the channelID to monitor for commands
--- main.go
+++ main.go
@@ -2,50 +2,118 @@
 package main
 
 import (
-	"flag"
-	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 )
 
-func init() {
-	flag.StringVar(&token, "t", "", "Bot Token")
-	flag.StringVar(&channelMonitor, "c", "", "Channel ID to monitor")
-	flag.Parse()
+var token string
+var channel string
+var guild string
+
+//temp
+var admins string
+var rolesStrings string
+var bannedusers string
+
+func readFiles() {
+	// token
+	tempBytes, err := ioutil.ReadFile("data/token")
+	if err != nil {
+		log.Printf("failed to read data/token file.")
+		return
+	}
+	token = string(tempBytes)
+
+	// channel
+	tempBytes, err = ioutil.ReadFile("data/channel")
+	if err != nil {
+		log.Printf("failed to read data/channel file.")
+		return
+	}
+	channel = string(tempBytes)
+
+	// guild
+	tempBytes, err = ioutil.ReadFile("data/guild")
+	if err != nil {
+		log.Printf("failed to read data/channel file.")
+		return
+	}
+	guild = string(tempBytes)
+
+	//admins
+	tempBytes, err = ioutil.ReadFile("data/admins")
+	if err != nil {
+		log.Printf("failed to read data/admins file.")
+		return
+	}
+	admins = string(tempBytes)
+
+	// roles
+	tempBytes, err = ioutil.ReadFile("data/roles")
+	if err != nil {
+		log.Printf("failed to read data/admins file.")
+		return
+	}
+	rolesStrings = string(tempBytes)
+
+	// banned users
+	tempBytes, err = ioutil.ReadFile("data/bans")
+	if err != nil {
+		log.Printf("failed to read data/admins file.")
+		return
+	}
+	bannedusers = string(tempBytes)
+
+	// temp output
+	log.Printf("token read '%s'", token)
+	log.Printf("channel read '%s'", channel)
+	log.Printf("guild read '%s'", guild)
+	log.Printf("admins read '%s'", admins)
+	log.Printf("roles read '%s'", rolesStrings)
+	log.Printf("bans read '%s'", bannedusers)
 }
 
-var token string
+// Format: <friendly print name> <user ID>
+func parseAdmins() {
 
-var channelMonitor string
+}
+
+// Format: <friendly print name> <user ID>
+func parseBans() {
+
+}
+
+// Format: <string variant> <role ID>
+func parseRoles() {
+
+}
 
 func main() {
 	log.Printf("Starting up")
 
-	errorNow := false
+	readFiles()
 
-	// TODO: READ files for secrets instead of commandline!
+	// Validation
 	if token == "" {
-		fmt.Println("No token provided. Please run: murderbot -t <bot token>")
-		errorNow = true
+		log.Fatalln("Token file was read but could not extract the token!")
 	}
-
-	if channelMonitor == "" {
-		fmt.Println("No channelID provided. Please run: muderbot -c <channelID>")
-		errorNow = true
+	if channel == "" {
+		log.Fatalln("Channel file was read but could not extract the channel ID!")
 	}
-
-	if errorNow {
-		return
+	if guild == "" {
+		log.Fatalln("Guild ID file was read but could not extract the guild ID!")
 	}
 
 	// Create a new Discord session using the provided bot token.
 	dg, err := discordgo.New("Bot " + token)
 	if err != nil {
-		fmt.Println("Error creating Discord session: ", err)
+		log.Println("Error creating Discord session: ", err)
 		return
 	}
 
@@ -57,19 +125,18 @@ func main() {
 	// Open a websocket connection to Discord and begin listening.
 	err = dg.Open()
 	if err != nil {
-		fmt.Println("error opening connection,", err)
+		log.Println("error opening connection,", err)
 		return
 	}
 
 	// Wait here until CTRL-C or other term signal is received.
-	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	log.Println("Bot is now running.  Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
 
 	// Cleanly close down the Discord session.
 	dg.Close()
-
 }
 
 // This function will be called (due to AddHandler above) when the bot receives
@@ -91,10 +158,16 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 
 	// Ignore all messages not in the specific bot channel
-	if m.ChannelID != channelMonitor {
-		log.Printf("Channel %s is not the right channel %s! (%s:%s)", m.ChannelID, channelMonitor, m.Author, m.Content)
+	if m.ChannelID != channel {
+		log.Printf("Channel %s is not the right channel %s! (%s:%s)", m.ChannelID, channel, m.Author, m.Content)
 		return
 	}
 
 	log.Printf("I saw the following message from %s in the %s channel: %s", m.Author, m.ChannelID, m.Content)
+
+	if strings.HasPrefix(m.Content, "!") {
+		// It's a command!
+		log.Println("Detected a command!")
+	}
+
 }
